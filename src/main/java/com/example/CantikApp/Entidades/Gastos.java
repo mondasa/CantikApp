a5b3e29fdeabf3c15e6f84a00c0b0bdb4680f9f7
mondasa <126993103+mondasa@users.noreply.github.com>
2024-06-01 21:55:29 -0600

    Update Gastos.java

--- src/main/java/com/example/CantikApp/Entidades/Gastos.java
+++ src/main/java/com/example/CantikApp/Entidades/Gastos.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.example.WafflyApi.Entidades;
+package com.example.CantikApp.Entidades;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
