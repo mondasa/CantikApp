23b46c9f79bc5c1d7c7c98e638bf32b3dd7a8e7d
mondasa <126993103+mondasa@users.noreply.github.com>
2024-06-01 21:55:57 -0600

    Update Productos.java

--- src/main/java/com/example/CantikApp/Entidades/Productos.java
+++ src/main/java/com/example/CantikApp/Entidades/Productos.java
@@ -1,5 +1,5 @@
 
-package com.example.WafflyApi.Entidades;
+package com.example.CantikApp.Entidades;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
